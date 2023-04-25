6f127c1d3a6e665dc7e8984f262eab55a5de2c7e
MiHi <minhhieuu26@gmail.com>
2023-04-26 00:04:47 +0700

    add unit test

---
+++ src/test/java/com/example/registrationms/controller/AuthenticationControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.registrationms.controller;
+
+import com.example.registrationms.dto.LoginRequest;
+import com.example.registrationms.service.AuthenticationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class AuthenticationControllerTest {
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Test
+    void login() {
+        var result = authenticationService.authenticate(new LoginRequest("MGV001", "123456"));
+        assertThat(result.getUsername()).isEqualTo("MGV001");
+        assertThat(result.getName()).isEqualTo("Nguyen Van A");
+    }
+}
